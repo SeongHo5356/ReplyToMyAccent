ab9461cacf88c5940a966ed41e4e9cf4f4dee743
Your Name <kjeng7897@gmail.com>
2024-05-16 11:36:22 +0900

    ADD : AdroidAPP Notification to Log

---
+++ DeployML/DeployML/SmplNotiTst/app/src/main/java/com/example/smplnotitst/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.smplnotitst
+
+import android.os.Bundle
+import android.service.notification.StatusBarNotification
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+import android.app.NotificationManager
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.provider.Settings
+import androidx.core.app.NotificationManagerCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if(!isNotificationPermissionGranted()){
+            startActivity(Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS))
+        }
+    }
+
+    private fun isNotificationPermissionGranted() : Boolean{
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1){
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            return notificationManager.isNotificationListenerAccessGranted(ComponentName(application,MyNotificationListenerService::class.java))
+        }
+        else{
+            return NotificationManagerCompat.getEnabledListenerPackages(applicationContext).contains(applicationContext.packageName)
+        }
+    }
+}
---
+++ DeployML/DeployML/SmplNotiTst/app/src/main/java/com/example/smplnotitst/MyNotificationListenerService.kt
@@ -0,0 +1,34 @@
+package com.example.smplnotitst
+
+import android.app.Notification
+import android.nfc.Tag
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.util.Log
+
+class MyNotificationListenerService : NotificationListenerService() {
+    override fun onNotificationPosted(sbn: StatusBarNotification?) {
+        super.onNotificationPosted(sbn)
+
+        val packageName: String = sbn?.packageName?:"Null"
+        val extras = sbn?.notification?.extras
+
+        val extraTitle: String = extras?.getString(Notification.EXTRA_TITLE).toString()
+        val extraText: String = extras?.get(Notification.EXTRA_TEXT).toString()
+        val extraBigText: String = extras?.get(Notification.EXTRA_BIG_TEXT).toString()
+        val extraInfoText: String = extras?.get(Notification.EXTRA_INFO_TEXT).toString()
+        val extraSubText: String = extras?.get(Notification.EXTRA_SUB_TEXT).toString()
+        val extraSummaryText: String = extras?.get(Notification.EXTRA_SUMMARY_TEXT).toString()
+
+        Log.d(
+            "TAG" , "onNotificationPosted:\n" +
+                    "PackageName: $packageName" +
+                    "Title: $extraTitle\n" +
+                    "Text: $extraText\n" +
+                    "BigText: $extraBigText\n" +
+                    "InfoText: $extraInfoText\n" +
+                    "SubText: $extraSubText\n" +
+                    "SummaryText: $extraSummaryText\n"
+        )
+    }
+}
